8919f3ed3e5c0f216239a953a32b8b736ccdf927
Juan Tapia <juant72@gmail.com>
2023-05-17 16:24:06 -0300

    Fixing chatt screen

---
+++ src/chatgptapp/lib/chat_screen.dart
@@ -0,0 +1,169 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'chatmessage.dart';
+import 'threedots.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  late OpenAI? chatGPT;
+  bool _isImageSearch = false;
+
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    chatGPT = OpenAI.instance.build(
+        token: dotenv.env["GPT_API_KEY"],
+        baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 6)));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    chatGPT?.close();
+    chatGPT?.genImgClose();
+    super.dispose();
+  }
+
+  // Link for api - https://beta.openai.com/account/api-keys
+
+  void _sendMessage() async {
+    if (_controller.text.isEmpty) return;
+    ChatMessage message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+      isImage: false,
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+      _isTyping = true;
+    });
+
+    _controller.clear();
+
+    if (_isImageSearch) {
+      final request = GenerateImage(message.text, 1, size: "256x256");
+
+      final response = await chatGPT!.generateImage(request);
+      Vx.log(response!.data!.last!.url!);
+      insertNewData(response.data!.last!.url!, isImage: true);
+    } else {
+      // final request =
+      //     CompleteText(prompt: message.text, model: kChatGptTurbo0301Model, maxTokens: 200);
+
+      // final response = await chatGPT!.onCompleteText(request: request);
+
+      //------------alternate------------
+      // final request = ChatCompleteText(messages: [
+      //   Map.of({"role": "user", "content": 'Hello!'})
+      // ], maxToken: 200, model: kChatGptTurbo0301Model);
+
+      // // prompt: message.text, maxTokens: 200, model: kChatGptTurbo);
+
+      // final response = chatGPT?.onChatCompletionStream(request: request);
+      //---------------------
+      final request = ChatCompleteText(messages: [
+        Map.of({"role": "user", "content": 'Hello!'})
+      ], maxToken: 200, model: kChatGptTurbo0301Model);
+
+      final response = await chatGPT?.onChatCompletion(request: request);
+      for (var element in response!.choices) {
+        print("data -> ${element.message.content}");
+      }
+
+      //** */
+      // print("data -> ", response);
+
+      Vx.log(response!.choices[0].message.toString());
+      insertNewData(response.choices[0].message.content, isImage: false);
+    }
+  }
+
+  void insertNewData(String response, {bool isImage = false}) {
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      sender: "bot",
+      isImage: isImage,
+    );
+
+    setState(() {
+      _isTyping = false;
+      _messages.insert(0, botMessage);
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            onSubmitted: (value) => _sendMessage(),
+            decoration: const InputDecoration.collapsed(
+                hintText: "Question/description"),
+          ),
+        ),
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                _isImageSearch = false;
+                _sendMessage();
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  _isImageSearch = true;
+                  _sendMessage();
+                },
+                child: const Text("Generate Image"))
+          ],
+        ),
+      ],
+    ).px16();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("ChatGPT & Dall-E2 Demo")),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Flexible(
+                  child: ListView.builder(
+                reverse: true,
+                padding: Vx.m8,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              )),
+              if (_isTyping) const ThreeDots(),
+              const Divider(
+                height: 1.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: context.cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+        ));
+  }
+}
